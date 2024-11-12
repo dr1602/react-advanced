4890851d6ccf1d47d0a34e023d379f7b0604f00a
dr1602 <david.arangelg@gmail.com>
2024-11-11 21:21:22 -0600

    11nov_24_prework

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,51 @@
 
 import './App.css'
-import { Header } from './components/Header'
-import { Hijo } from './components/Hijo'
+import { useState } from 'react'
 import { UserProvider } from './providers/UserProviders'
 import { ProductProvider } from './providers/ProductsProviders'
+import { Header } from './components/Header'
+import { Hijo } from './components/Hijo'
+import { PulsaBotones } from './components/PulsaBotonUseRef'
 
 function App() {
+  const [isContext, setIsContext] = useState<boolean>(false)
+  const [isRef, setIsRef] = useState<boolean>(false)
+
+  const toggleIsContext = () => {
+    setIsContext(!isContext)
+    setIsRef(false)
+  }
+
+  const toggleRef = () => {
+    setIsContext(false)
+    setIsRef(!isRef)
+  }
 
   return (
     <>
+      <div className='App__menu'>
+        <button
+          onClick={toggleIsContext}
+        >
+          useContext
+        </button>
+        <button
+          onClick={toggleRef}
+        >
+          useRef
+        </button>
+      </div>
       <UserProvider>
         <ProductProvider>
           <Header/>
           <div className='App'>
-            <Hijo />
+            {
+              !!isContext && <Hijo />
+            }
+            {
+              !!isRef && <PulsaBotones/>
+            }
+            
           </div>
         </ProductProvider>
       </UserProvider>
