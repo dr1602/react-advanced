6026804bb18217acd92239c89fdf65412bd01e56
dr1602 <david.arangelg@gmail.com>
2024-11-26 21:24:42 -0600

    fetchInmediateFetch

--- src/components/ApiFetch/ApiFetch.tsx
+++ src/components/ApiFetch/ApiFetch.tsx
@@ -15,7 +15,9 @@ interface BerryProps {
 }
 
 export const ApiFetch: React.FC<ApiFetchProps> = ({ id }) => {
-    const [dataFetch, setDataFetch] = useState<BerryProps>()
+    const [dataFetchArrow, setDataFetchArrow] = useState<BerryProps>()
+    const [dataFetchFunc, setDataFetchFunc] = useState<BerryProps>()
+    const [dataFetchInm, setDataFetchInm] = useState<BerryProps>()
 
     const fetchData = useCallback (async () => {
         try {
@@ -26,9 +28,7 @@ export const ApiFetch: React.FC<ApiFetchProps> = ({ id }) => {
             }
 
             const data = await response.json()
-            console.log(data)
-            setDataFetch(data)
-
+            setDataFetchArrow(data)
         } catch(error) {
             console.error('Error:', error)
         }
@@ -36,20 +36,75 @@ export const ApiFetch: React.FC<ApiFetchProps> = ({ id }) => {
         
     },[id])
 
+    async function fetchDataFunc() {
+        try {
+            const response = await fetch(`https://pokeapi.co/api/v2/berry/${id+1}`)
+
+            if(!response.ok){
+                throw new Error('Error en la solicitud')
+            }
+
+            const data = await response.json()
+            setDataFetchFunc(data)
+        } catch (error) {
+            console.log('Error:', error)
+        }
+    }
+
+    (async () => {
+        try{
+            const response = await fetch(`https://pokeapi.co/api/v2/berry/${id+2}`)
+
+            if(!response.ok) {
+                throw new Error('Error en la solicitud')
+            }
+
+            const data = await response.json()
+            setDataFetchInm(data)
+        } catch(error) {
+            console.error('Error:', error)
+        }
+    })();
+
     useEffect(() => {
         fetchData()
+        fetchDataFunc()
     },[fetchData])
 
     return (
         <>
             <div>
-                <h2> Berries: </h2>
+                <h2> Berries from Arrow: </h2>
 
                     <div className='Berries'>
-                        <h3 className='Berries__Title'> {dataFetch?.name} </h3>
+                        <h3 className='Berries__Title'> {dataFetchArrow?.name} </h3>
                         <span className='Berries__GrowthTime'>
                             <strong className='Berries__GrowthTime--title'> Growth Time: &nbsp; </strong>
-                            <p className='Berries__GrowthTime--value'> {dataFetch?.growth_time} </p>
+                            <p className='Berries__GrowthTime--value'> {dataFetchArrow?.growth_time} </p>
+                        </span>
+                    </div>
+            </div>
+
+            <div>
+                <h2> Berries from Func: </h2>
+
+                    <div className='Berries'>
+                        <h3 className='Berries__Title'> {dataFetchFunc?.name} </h3>
+                        <span className='Berries__GrowthTime'>
+                            <strong className='Berries__GrowthTime--title'> Growth Time: &nbsp; </strong>
+                            <p className='Berries__GrowthTime--value'> {dataFetchFunc?.growth_time} </p>
+                        </span>
+                    </div>
+            </div>
+
+            <div>
+                <h2> Berries from Inmediate Call: </h2>
+
+                    <div className='Berries'>
+                        <h3 className='Berries__Title'> {dataFetchInm?.name} </h3>
+                        <span className='Berries__GrowthTime'>
+                            <strong className='Berries__GrowthTime--title'> Growth Time: &nbsp; </strong>
+                            <p className='Berries__GrowthTime--value'> {dataFetchInm?.growth_time} </p>
                         </span>
                     </div>
             </div>
