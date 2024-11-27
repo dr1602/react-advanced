735010dc7158a201fba1d4844b612291e4d589bd
dr1602 <david.arangelg@gmail.com>
2024-11-27 14:03:00 -0600

    fetch_esq

--- src/components/ApiFetch/ApiFetch.tsx
+++ src/components/ApiFetch/ApiFetch.tsx
@@ -18,8 +18,29 @@ export const ApiFetch: React.FC<ApiFetchProps> = ({ id }) => {
     const [dataFetchArrow, setDataFetchArrow] = useState<BerryProps>()
     const [dataFetchFunc, setDataFetchFunc] = useState<BerryProps>()
     const [dataFetchInm, setDataFetchInm] = useState<BerryProps>()
+    const [dataFetchCall, setDataFetchCall] = useState<BerryProps>()
+    const [fetchStateArrow, setFetchStateArrow] = useState({
+        error: '',
+        isLoading: false,
+    })
+    const [fetchStateFunc, setFetchStateFunc] = useState({
+        error: '',
+        isLoading: false,
+    })
+    const [fetchStateInm, setFetchStateInm] = useState({
+        error: '',
+        isLoading: false,
+    })
+    const [fetchStateCall, setFetchStateCall] = useState({
+        error: '',
+        isLoading: false,
+    })
 
     const fetchData = useCallback (async () => {
+        setFetchStateArrow((prevState) => ({
+            ...prevState,
+            isLoading: !prevState.isLoading,
+        }))
         try {
             const response = await fetch(`https://pokeapi.co/api/v2/berry/${id}`);
 
@@ -29,14 +50,28 @@ export const ApiFetch: React.FC<ApiFetchProps> = ({ id }) => {
 
             const data = await response.json()
             setDataFetchArrow(data)
+
         } catch(error) {
-            console.error('Error:', error)
+            setFetchStateArrow((prevState) => ({
+                ...prevState,
+                error: String(error),
+            }))
+        } finally {
+            setFetchStateArrow((prevState) => ({
+                ...prevState,
+                isLoading: !prevState.isLoading,
+            }))
         }
 
         
     },[id])
 
     async function fetchDataFunc() {
+        setFetchStateFunc((prevState) => ({
+            ...prevState,
+            isLoading: !prevState.isLoading,
+        }))
+
         try {
             const response = await fetch(`https://pokeapi.co/api/v2/berry/${id+1}`)
 
@@ -47,11 +82,24 @@ export const ApiFetch: React.FC<ApiFetchProps> = ({ id }) => {
             const data = await response.json()
             setDataFetchFunc(data)
         } catch (error) {
-            console.log('Error:', error)
+            setFetchStateFunc((prevState) => ({
+                ...prevState,
+                error: String(error),
+            }))
+        } finally {
+            setFetchStateFunc((prevState) => ({
+                ...prevState,
+                isLoading: !prevState.isLoading,
+            }))
         }
     }
 
     (async () => {
+        setFetchStateInm((prevState) => ({
+            ...prevState,
+            isLoading: !prevState.isLoading,
+        }))
+
         try{
             const response = await fetch(`https://pokeapi.co/api/v2/berry/${id+2}`)
 
@@ -61,8 +109,17 @@ export const ApiFetch: React.FC<ApiFetchProps> = ({ id }) => {
 
             const data = await response.json()
             setDataFetchInm(data)
+
         } catch(error) {
-            console.error('Error:', error)
+            setFetchStateInm((prevState) => ({
+                ...prevState,
+                error: String(error),
+            }))
+        } finally {
+            setFetchStateInm((prevState) => ({
+                ...prevState,
+                isLoading: !prevState.isLoading,
+            }))
         }
     })();
 
@@ -71,42 +128,119 @@ export const ApiFetch: React.FC<ApiFetchProps> = ({ id }) => {
         fetchDataFunc()
     },[fetchData])
 
+    const fetchDataCall = useCallback(async () => {
+        setFetchStateCall((prevState) => ({
+            ...prevState,
+            isLoading: !prevState.isLoading,
+        }))
+
+        try {
+            const response = await fetch(`https://pokeapi.co/api/v2/berry/${id+3}`)
+
+            if(!response.ok) {
+                throw new Error('Error en la solicitud')
+            }
+
+            const data = await response.json();
+            
+            setDataFetchCall(data)
+        } catch (error) {
+            setFetchStateCall((prevState) => ({
+                ...prevState,
+                error: String(error),
+            }))
+        } finally {
+            setFetchStateCall((prevState) => ({
+                ...prevState,
+                isLoading: !prevState.isLoading,
+            }))
+        }
+    },[id])
+
+    useEffect(() => {
+        fetchDataCall()
+    },[fetchDataCall])
+
     return (
         <>
             <div>
                 <h2> Berries from Arrow: </h2>
 
-                    <div className='Berries'>
-                        <h3 className='Berries__Title'> {dataFetchArrow?.name} </h3>
-                        <span className='Berries__GrowthTime'>
-                            <strong className='Berries__GrowthTime--title'> Growth Time: &nbsp; </strong>
-                            <p className='Berries__GrowthTime--value'> {dataFetchArrow?.growth_time} </p>
-                        </span>
-                    </div>
+                {
+                    !fetchStateArrow.isLoading ? (
+                        <div className='Berries'>
+                            <h3 className='Berries__Title'> {dataFetchArrow?.name} </h3>
+                            <span className='Berries__GrowthTime'>
+                                <strong className='Berries__GrowthTime--title'> Growth Time: &nbsp; </strong>
+                                <p className='Berries__GrowthTime--value'> {dataFetchArrow?.growth_time} </p>
+                            </span>
+                        </div>
+                    ) : (
+                        <>
+                        </>
+                    )
+                }
+
             </div>
 
             <div>
                 <h2> Berries from Func: </h2>
 
-                    <div className='Berries'>
-                        <h3 className='Berries__Title'> {dataFetchFunc?.name} </h3>
-                        <span className='Berries__GrowthTime'>
-                            <strong className='Berries__GrowthTime--title'> Growth Time: &nbsp; </strong>
-                            <p className='Berries__GrowthTime--value'> {dataFetchFunc?.growth_time} </p>
-                        </span>
-                    </div>
+                {
+                    !fetchStateFunc.isLoading ? (
+                        <div className='Berries'>
+                            <h3 className='Berries__Title'> {dataFetchFunc?.name} </h3>
+                            <span className='Berries__GrowthTime'>
+                                <strong className='Berries__GrowthTime--title'> Growth Time: &nbsp; </strong>
+                                <p className='Berries__GrowthTime--value'> {dataFetchFunc?.growth_time} </p>
+                            </span>
+                        </div>
+                    ) : (
+                        <>
+                        </>
+                    )
+                }
+
             </div>
 
             <div>
                 <h2> Berries from Inmediate Call: </h2>
 
-                    <div className='Berries'>
-                        <h3 className='Berries__Title'> {dataFetchInm?.name} </h3>
-                        <span className='Berries__GrowthTime'>
-                            <strong className='Berries__GrowthTime--title'> Growth Time: &nbsp; </strong>
-                            <p className='Berries__GrowthTime--value'> {dataFetchInm?.growth_time} </p>
-                        </span>
-                    </div>
+                {
+                    !fetchStateInm.isLoading ? (
+                        <div className='Berries'>
+                            <h3 className='Berries__Title'> {dataFetchInm?.name} </h3>
+                            <span className='Berries__GrowthTime'>
+                                <strong className='Berries__GrowthTime--title'> Growth Time: &nbsp; </strong>
+                                <p className='Berries__GrowthTime--value'> {dataFetchInm?.growth_time} </p>
+                            </span>
+                        </div>
+                    ) : (
+                        <>
+                        </>
+                    )
+                }
+
+            </div>
+
+            <div>
+                <h2> Berries from Callback: </h2>
+
+                {
+                    !fetchStateCall.isLoading ? (
+                        <div className='Berries'>
+                            <h3 className='Berries__Title'> {dataFetchCall?.name} </h3>
+                            <span className='Berries__GrowthTime'>
+                                <strong className='Berries__GrowthTime--title'> Growth Time: &nbsp; </strong>
+                                <p className='Berries__GrowthTime--value'> {dataFetchCall?.growth_time} </p>
+                            </span>
+                        </div>
+                    ) : (
+                        <>
+                        </>
+                    )
+                }
+
             </div>
         </>
     )
